013c58965164ab3f939dfb65692bc4d00107d99a
Mick Thompson <dthompson@gmail.com>
2012-10-20 21:44:06 -0700

    only run map setup once

--- static/map.js
+++ static/map.js
@@ -3,47 +3,40 @@ if(Meteor.isClient) {
   var vecMarker = null;
   var userMarker = null;
 
-  Template.map.created = function() {
+  function watchVehicle() {
+    var vehicle = Vehicles.findOne();
+
+    if (!vehicle) {
+      Meteor.setTimeout(watchVehicle, 5000);
+      return;
+    }
+
+    if (vecMarker === null) {
+      vecMarker = new google.maps.Marker({
+        map: muniMap.map,
+        position: new google.maps.LatLng(vehicle.lat, vehicle.lon),
+        title: "Vehicle"
+      });
+    } else {
+      vecMarker.position = new google.maps.LatLng(vehicle.lat, vehicle.lon);
+    }
+
+    var bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(vehicle.lat, vehicle.lon),
+      new google.maps.LatLng(userMarker.position.Xa, userMarker.position.Ya)
+    );
+    muniMap.map.fitBounds(bounds);
 
 
-      //Session.set("vehicle", 5481);
-
-      function watchVehicle() {
-        console.log(userMarker);
-        var vehicle = Vehicles.findOne();
-        console.log(vehicle);
-
-        if (!vehicle) {
-          Meteor.setTimeout(watchVehicle, 5000);
-          return;
-        }
-
-        if (vecMarker === null) {
-          vecMarker = new google.maps.Marker( {
-            map: muniMap.map,
-            position: new google.maps.LatLng(vehicle.lat, vehicle.lon),
-            title: "Vehicle"
-          });
-        } else {
-          vecMarker.position = new google.maps.LatLng(vehicle.lat, vehicle.lon);
-        }
-
-        var bounds = new google.maps.LatLngBounds(
-          new google.maps.LatLng(vehicle.lat, vehicle.lon),
-          new google.maps.LatLng(userMarker.position.Xa, userMarker.position.Ya)
-        );
-        muniMap.map.fitBounds(bounds);
-
-
-        Meteor.setTimeout(watchVehicle, 5000);
-      }
-      watchVehicle();
-
+    Meteor.setTimeout(watchVehicle, 5000);
   }
-
+ 
   var muniMap = {
-
+    firstRun:true,
     init:function(){
+
+      if(!muniMap.firstRun)
+        return;
       var myOptions = {
         center: new google.maps.LatLng(37.765, -122.443),
         zoom: 15,
@@ -54,14 +47,12 @@ if(Meteor.isClient) {
       var location = new google.maps.LatLng(37.765, -122.443);
       if(Session.get("location"))
         location = new google.maps.LatLng(Session.get("location")[1], Session.get("location")[0]);
-      
-      console.log("loc", location);
 
       muniMap.map = new google.maps.Map($("#map")[0], myOptions);
       muniMap.userMarker = new google.maps.Marker( {
         position: location,
         title: "You"
-      });
+      }); 
 
       if(navigator.geolocation) {
         var wpid = navigator.geolocation.watchPosition(function(position) {
@@ -77,6 +68,11 @@ if(Meteor.isClient) {
           console.log("error", e);
         }, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
       }
+      muniMap.firstRun = false;
+    },
+    afterRender:function(){
+
+
     }
 
   }
@@ -108,6 +104,7 @@ if(Meteor.isClient) {
   };
 
   Template.map.rendered = muniMap.init;
+  Template.map.created = watchVehicle;
   //created.
 
 }
